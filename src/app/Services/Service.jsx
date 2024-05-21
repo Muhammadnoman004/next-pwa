d503ef98c318c09485e278b3800cd12f56c75092
Noman Shamim <nomanshamim2004@gmail.com>
2024-05-21 22:47:29 +0500

    Service section done.

--- src/app/Services/Service.jsx
+++ src/app/Services/Service.jsx
@@ -5,64 +5,64 @@ import { Card, Col, Container, Row } from 'react-bootstrap'
 export default function Service() {
     return (
         <Container >
-            <div className='mt-4 mb-5'>
-                <h1 className='text-center'>Our Services</h1>
+            <div className='p-3 mt-4 mb-5' >
+                <h1 className='text-center' style={{ fontWeight: "bold" }}>Our Services</h1>
                 <Row>
                     <Col xs={12} sm={6} md={4}>
-                        <Card style={{ marginTop: "20px" }}>
+                        <Card style={{ marginTop: "20px", minHeight: "250px" }} bg='dark' text='light'>
                             <Card.Body>
-                                <Card.Title>Web Design</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='text-primary text-center'>Web Design</Card.Title><hr />
+                                <Card.Text className='text-center'>
                                     Web design refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} sm={6} md={4}>
-                        <Card style={{ marginTop: "20px" }}>
+                        <Card style={{ marginTop: "20px", minHeight: "250px" }} bg='dark' text='light'>
                             <Card.Body>
-                                <Card.Title>Digital Marketing</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='text-primary text-center'>Digital Marketing</Card.Title><hr />
+                                <Card.Text className='text-center'>
                                     Degital marketing refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} sm={6} md={4}>
-                        <Card style={{ marginTop: "20px" }}>
+                        <Card style={{ marginTop: "20px", minHeight: "250px" }} bg='dark' text='light'>
                             <Card.Body>
-                                <Card.Title>Photography</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='text-primary text-center'>Photography</Card.Title><hr />
+                                <Card.Text className='text-center'>
                                     Photography refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} sm={6} md={4}>
-                        <Card style={{ marginTop: "20px" }}>
+                        <Card style={{ marginTop: "20px", minHeight: "250px" }} bg='dark' text='light'>
                             <Card.Body>
-                                <Card.Title>Graphic Designing</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='text-primary text-center'>Graphic Designing</Card.Title><hr />
+                                <Card.Text className='text-center'>
                                     Graphic designing refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} sm={6} md={4}>
-                        <Card style={{ marginTop: "20px" }}>
+                        <Card style={{ marginTop: "20px", minHeight: "250px" }} bg='dark' text='light'>
                             <Card.Body>
-                                <Card.Title>Web Development</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='text-primary text-center'>Web Development</Card.Title><hr />
+                                <Card.Text className='text-center'>
                                     Web development refers to the design of websites. It usually refers to the user experience aspects of website development rather than software development.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} sm={6} md={4}>
-                        <Card style={{ marginTop: "20px" }}>
+                        <Card style={{ marginTop: "20px", minHeight: "250px" }} bg='dark' text='light'>
                             <Card.Body>
-                                <Card.Title>MS Office</Card.Title>
-                                <Card.Text>
+                                <Card.Title className='text-primary text-center'>MS Office</Card.Title><hr />
+                                <Card.Text className='text-center'>
                                     MS office refers to the design of websites. It usually formula refers to the user experience aspects of website development rather than software development.
                                 </Card.Text>
                             </Card.Body>
